6e4a8543c522f88dd1d96452a064e3595ac0b6b4
Daniel <danisanjose60@gmail.com>
2026-05-14 02:53:56 -0400

    Segundo Casi

--- obj/Debug/net8.0/OvejaNegra.AssemblyInfo.cs
+++ obj/Debug/net8.0/OvejaNegra.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OvejaNegra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6a9f3523245f014fec81929b492363554dd435e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+806896346373bb1a58481df654c9ded245948cec")]
 [assembly: System.Reflection.AssemblyProductAttribute("OvejaNegra")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OvejaNegra")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
